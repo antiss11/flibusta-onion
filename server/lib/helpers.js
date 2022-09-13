e9381998d47045c28a70178ed4de674f7b96a795
Dmitry Kulik <recc1337@gmail.com>
2022-09-13 18:31:37 +0300

    Add function to get books data

---
+++ server/lib/helpers.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const FlibustaAPI = require("flibusta").default;
+const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent;
+
+
+const MIMES = {
+  "application/fb2+zip": 'zip',
+  "application/html+zip": 'zip',
+  "application/x-mobipocket-ebook": 'mobi',
+  "application/rtf+zip": 'zip',
+  "application/txt+zip": 'zip'
+};
+
+const ORIGIN = "http://flibustaongezhld6dibs2dps6vm4nvqg2kp7vgowbu76tzopgnhazqd.onion"
+
+const httpAgent = new SocksProxyAgent('socks5h://127.0.0.1:9050');
+
+async function getInfo(bookTitle) {
+	const flibustaApi = new FlibustaAPI(ORIGIN, { httpAgent });
+	const result = await flibustaApi.getBooksByNameFromOpds(bookTitle);
+  const covers64 = result.map(book => getImage(book.cover));
+
+  return Promise.all(covers64).then(covers64 => {
+    return result.map((book, i) => ({...book, 
+      cover64: covers64[i],
+      downloads: book.downloads.map(downloadInfo => ({
+        name: downloadInfo.link.split('/').at(-1), 
+        link: new URL(path.join(ORIGIN, downloadInfo.link)),
+        extension: MIMES[downloadInfo.type]})),
+      author: book.author[0].name,
+    }));
+  });
+}
