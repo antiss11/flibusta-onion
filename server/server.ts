3df01204515c6a7fed664d47267d5d760acfc44d
Dmitry Kulik <recc1337@gmail.com>
2022-09-13 18:39:07 +0300

    Add files downloading logic to backend

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const books = require('./lib/search');
+const helpers = require('./lib/helpers');
+const path = require('path');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/search', async (req, res) => {
 	const query = req.body.query;
-	const booksData = await books.getInfo(query);
+	const booksData = await helpers.getInfo(query);
 	res.send(booksData);
+})
+
+app.get('/download/:url', async (req, res) => {
+	const url = req.params.url;
+	const fileName = await helpers.downloadFile(url);
+	res.download('public/' + fileName);
 })
 
 app.listen(3001, () => console.log('express in running'));
