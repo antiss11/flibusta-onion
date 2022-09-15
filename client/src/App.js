1c2c062f3180992be4af5a419d8501c86576804a
Dmitry Kulik <recc1337@gmail.com>
2022-09-15 05:55:40 +0300

    Fix spaces, add progress animation, change state structure of App

--- client/src/App.js
+++ client/src/App.js
@@ -7,84 +7,158 @@ import Search from "./Components/Search/Search";
 import Book from "./Components/Book/Book";
 import CyrillicToTranslit from "cyrillic-to-translit-js";
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { query: "", books: {}, isSearching: false };
 
-	constructor(props) {
-		super(props);
-		this.state = {query: "", books: []};
-		
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.handleDownload = this.handleDownload.bind(this);
-	}
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDownload = this.handleDownload.bind(this);
+  }
 
-	async handleSubmit(e) {
-		e.preventDefault();
-		await this._search();
-	}
+  async handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ isSearching: true });
+    await this._search();
+  }
 
-	handleChange(e) {
-		this.setState({query: e.target.value});
-	}
+  handleChange(e) {
+    this.setState({ query: e.target.value });
+  }
 
-	async _search() {		
-		const response = await fetch('http://localhost:3001/search', {
-			method: 'POST',
-			redirect: 'follow',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(this.state),
-		});
+  async _search() {
+    const response = await fetch("http://localhost:3001/search", {
+      method: "POST",
+      redirect: "follow",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    });
 
-		const books = await response.json();
-		this.setState({ books });
-	}
+    const books = await response.json();
 
-	async handleDownload(e) {
-			e.preventDefault();		
-			const book = e.target.closest('.book');		
-			const author = book.querySelector('.book__author').textContent;
-			const title = book.querySelector('.book__title').textContent;
-			const ebookType = e.target.textContent
-			const ext = e.target.dataset.ext;
-			const cyrillicToTranslit = new CyrillicToTranslit();
-			const fileName = cyrillicToTranslit.transform(`${author} ${title} ${ebookType}.${ext}`, '_');
-			await this._download(e.target.href, fileName);
-	}	
+    // Create an object with keys as names (fb2, mobi, etc.)
+    const formattedBooksArray = books.map((book) => {
+      const downloadsInfo = Object.fromEntries(
+        book.downloads.map((downloadInfo) => [
+          downloadInfo.name,
+          { ...downloadInfo, isDownloading: false },
+        ])
+      );
+      return {
+        ...book,
+        downloads: downloadsInfo,
+      };
+    });
 
-	async _download(url, fileName) {
-		axios.get('http://localhost:3001/download/' + encodeURIComponent(url), {responseType: 'blob'}).then(res => {
-			const url = window.URL.createObjectURL(new Blob([res.data]));
-			const link = document.createElement('a');
-			link.href = url;
-			link.setAttribute('download', fileName);
-			document.body.appendChild(link);
-			link.click();
-		}).catch(err => console.log(err));
-	}
+    // Creating an object with usage array indexes as ids
+    const formattedBooksObj = Object.fromEntries(
+      Object.entries(formattedBooksArray)
+    );
 
+    this.setState({ books: formattedBooksObj, isSearching: false });
+  }
 
-	render() {
-		const gridItems = this.state.books.map(({title, cover64, author, downloads}) => (
-			<Grid item xs={4}>
-				<Book title={title} cover={cover64} author={author} downloadLinks={downloads} handleDownload={this.handleDownload} />
-			</Grid>
-		))
+  toggleLinkDownloading(id, linkName) {
+    this.setState((prev) => {
+      const books = Object.assign(prev.books);
+      const isDownloading = books[id].downloads[linkName].isDownloading;
+      books[id].downloads[linkName].isDownloading = !isDownloading;
+      return {
+        ...prev,
+        books,
+      };
+    });
+  }
 
-		return (
-			<Grid container spacing={2} justifyItems="center" alignItems="center" px={7}>
-				<Grid item container xs={12} alignItems="center" justifyItems="center">
-					<Search onSubmit={this.handleSubmit} onChange={this.handleChange} value={this.state.value}/>
-				</Grid>
-				<Grid item container spacing={5}>		
-					{gridItems}					
-				</Grid>
-				
-			</Grid>
-		)
-	}
+  async handleDownload(e) {
+    e.preventDefault();
+    const book = e.target.closest(".book");
+    const id = book.id;
+    const author = book.querySelector(".book__author").textContent;
+    const title = book.querySelector(".book__title").textContent;
+    const ebookType = e.target.textContent;
+    const ext = e.target.dataset.ext;
+    const linkName = e.target.textContent;
+    this.toggleLinkDownloading(id, linkName);
+    const cyrillicToTranslit = new CyrillicToTranslit();
+    const fileName = cyrillicToTranslit.transform(
+      `${author} ${title} ${ebookType}.${ext}`,
+      "_"
+    );
+    this._download(e.target.href, fileName).then((_) => {
+      this.toggleLinkDownloading(id, linkName);
+    });
+  }
+
+  _download(url, fileName) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get("http://localhost:3001/download/" + encodeURIComponent(url), {
+          responseType: "blob",
+        })
+        .then((res) => {
+          const url = window.URL.createObjectURL(new Blob([res.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", fileName);
+          document.body.appendChild(link);
+          link.click();
+          resolve();
+        })
+        .catch((err) => console.log(err));
+    });
+  }
+
+  render() {
+    const gridItems = Object.keys(this.state.books).map((id) => {
+      const { title, cover64, author, downloads } = this.state.books[id];
+      return (
+        <Grid item md={6} lg={3} key={id}>
+          <Book
+            title={title}
+            cover={cover64}
+            author={author}
+            downloadLinks={downloads}
+            handleDownload={this.handleDownload}
+            id={id}
+          />
+        </Grid>
+      );
+    });
+
+    return (
+      <Grid
+        container
+        spacing={2}
+        justifyItems="center"
+        justifyContent="center"
+        alignItems="center"
+        px={7}
+      >
+        <Grid
+          item
+          container
+          xs={12}
+          alignItems="center"
+          justifyItems="center"
+          justifyContent="center"
+        >
+          <Search
+            onSubmit={this.handleSubmit}
+            onChange={this.handleChange}
+            value={this.state.value}
+            isSearching={this.state.isSearching}
+          />
+        </Grid>
+        <Grid item container spacing={5}>
+          {gridItems}
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
