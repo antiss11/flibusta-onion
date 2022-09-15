c33ecba6e51c22f63c100a0f941d9d9552f8fafb
Dmitry Kulik <recc1337@gmail.com>
2022-09-15 06:06:12 +0300

    Fix spaces, refactoring Book

--- client/src/Components/Book/Book.js
+++ client/src/Components/Book/Book.js
@@ -1,45 +1,64 @@
 import React from "react";
-import { ButtonGroup, Button, Card, CardActions, CardMedia } from "@mui/material";
-import {Typography} from "@mui/material";
-import {CircularProgress} from "@mui/material";
+import {
+  ButtonGroup,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
 import coverPlaceholder from "../../images/cover-placeholder.svg";
+import DownloadButton from "../DownloadButton/DownloadButton";
 
 const cardStyles = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-}
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+};
 
-function Book({title, cover, author, downloadLinks, handleDownload}) {
-  const linksNames = [];
-  const urls = [];
-
-  for (const [linkName, linkInfo] of Object.entries(downloadLinks)) {
-    linksNames.push(linkName);
-    urls.push(linkInfo.url);
-  }
-  
-  const downloadButtons = linksNames.map((linkName, i) => (
-    <Button href={urls[i]} onClick={handleDownload}>{linkName}</Button>
+function Book({ title, cover, author, downloadLinks, handleDownload, id }) {
+  const downloadButtons = Object.values(downloadLinks).map((value, i) => (
+    <DownloadButton
+      text={value.name}
+      onClick={handleDownload}
+      href={value.link}
+      extension={value.extension}
+      key={i}
+      isDownloading={value.isDownloading}
+    />
   ));
 
-  
-  cover = (cover === null) ? coverPlaceholder : `data:image/jpg;base64, ${cover}`;
-  
+  const coverImg =
+    cover === null ? coverPlaceholder : `data:image/jpg;base64, ${cover}`;
+
   return (
-    <Card className="book" sx={cardStyles} px={3}>
-      <CardMedia component="img" src={cover} sx={{maxWidth: "250px", width: 'auto'}}/>
-      <Typography className="book__title" align="center" sx={{marginTop: 'auto'}}>{title}</Typography>
-      <Typography className="book__author" align="center">{author}</Typography>
+    <Card className="book" sx={cardStyles} px={3} id={id}>
+      <CardMedia
+        component="img"
+        src={coverImg}
+        sx={{ maxWidth: "250px", width: "auto" }}
+      />
+      <Typography
+        className="book__title"
+        align="center"
+        sx={{ marginTop: "auto" }}
+        variant="h6"
+      >
+        {title}
+      </Typography>
+      <Typography className="book__author" align="center" variant="string">
+        {author}
+      </Typography>
       <CardActions>
-        <ButtonGroup variant="outlined">
+        <ButtonGroup size="small" variant="outlined">
           {downloadButtons}
         </ButtonGroup>
       </CardActions>
     </Card>
-  )
+  );
 }
 
 export default Book;
