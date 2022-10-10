e032fbe5de8d9b40e5668259f4f580eaf1a0a111
Dmitry Kulik <recc1337@gmail.com>
2022-10-10 08:52:30 +0300

    Fix server's imports, make `helpers` function calls correct

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import helpers from './lib/helpers';
+import {getBookInfo, downloadFile} from './lib/helpers';
 import path from 'path';
 import bodyParser from 'body-parser';
 import { SocksProxyAgent } from 'socks-proxy-agent';
@@ -16,14 +16,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/search', async (req, res) => {
-  const book = req.query.book;
-  const booksData = await helpers.getInfo(book);
+  const bookTitle = req.query.book;
+  const booksData = await getBookInfo(flibustaOnionOrigin, httpAgent, bookTitle);
   res.send(booksData);
 });
 
 app.get('/download/:url', async (req, res) => {
   const url = req.params.url;
-  const fileName = await helpers.downloadFile(url);
+  const fileName = await downloadFile(flibustaOnionOrigin, httpAgent, url);
   res.download('public/' + fileName);
 });
 
