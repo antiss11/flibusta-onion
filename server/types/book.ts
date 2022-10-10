6e70e2b4ef90ead35592f61616fd468afc8bb776
Dmitry Kulik <recc1337@gmail.com>
2022-10-10 10:24:09 +0300

    Fix `Book` type fields

--- server/types/book.ts
+++ server/types/book.ts
@@ -3,8 +3,9 @@ import bookDownload from "./bookDownload";
 interface Book {
   description: string;
   title: string;
-  cover64: string;
-  downloads: Array<bookDownload>
+  cover64: string | null | undefined;
+  downloads: Array<bookDownload>;
+  author: string;
 }
 
 export default Book;
