e593b90e508c5b695c74ec719f9ef06d6bff4cc4
antiss11 <recc1337@gmail.com>
2023-06-01 12:58:35 +0300

    Lint server's files

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,3 @@
-import app from "./src/app";
+import app from './src/app';
 
-app.listen(3001, () => {console.log('express is running')});
+app.listen(3001, () => {console.log('express is running');});
--- server/types/mimes.ts
+++ server/types/mimes.ts
@@ -2,14 +2,14 @@ type Extension = string;
 
 type Mimes = {
   [mime: string]: Extension
-}
+};
 
 const mimes: Mimes = {
-  "application/fb2+zip": "zip",
-  "application/html+zip": "zip",
-  "application/x-mobipocket-ebook": "mobi",
-  "application/rtf+zip": "zip",
-  "application/txt+zip": "zip",
+  'application/fb2+zip': 'zip',
+  'application/html+zip': 'zip',
+  'application/x-mobipocket-ebook': 'mobi',
+  'application/rtf+zip': 'zip',
+  'application/txt+zip': 'zip',
 };
 
 export default mimes;
